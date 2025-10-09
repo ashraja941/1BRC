75979f6ee921d5f29ac81671a918f1a5c456f6b4
ash <ashraja941@gmail.com>
2025-10-09 13:16:22 -0500

    can read in file

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() !void {
-    std.debug.print("Starting", .{});
+    const cwd = std.fs.cwd();
+    const measurementData = try cwd.openFile("../data/measurements.txt", .{ .mode = .read_only });
+    defer measurementData.close();
+
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // const allocator = gpa.allocator();
+
+    var buf: [1024]u8 = undefined;
+    var fileReader = measurementData.reader(&buf);
+    const reader = &fileReader.interface;
+    const maybeLine = try reader.takeDelimiterExclusive('\n');
+
+    print("{s}", .{maybeLine});
 }
